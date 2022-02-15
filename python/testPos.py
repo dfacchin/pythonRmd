e2534a72a4cbd7006db3ac1ab819fca086d6c3f8
Florian9638 <florian.dallago@aigritec.com>
2022-02-15 11:22:35 +0100

    add comments

--- python/testPos.py
+++ python/testPos.py
@@ -4,47 +4,58 @@ import time
 # Using specific buses works similar:
 bus = can.interface.Bus(bustype='socketcan', channel='can0', bitrate=1000000)
 
-#RMD motor with ID 1
-motor1 = RMD.RMD(0x142,bus,ratio = 13) #elbow
 
-motor1.info()
+# ---------- RMD motor with ID 1 (Elbow) ----------
+motor1 = RMD.RMD(0x142,bus,ratio = 13) # Elbow
 
-motor1.Fn30()
-motor1.Fn33()
+motor1.info() # 'info' prints Pid and Acceleration 
+
+motor1.Fn30() # read PID
+motor1.Fn33() # read acceleration
 motor1.info()
-motor1.PidPosKp  = 30
+# Specify desired PID
+motor1.PidPosKp  = 30 
 motor1.PidPosKi  = 0
 motor1.PidVelKp  = 30
 motor1.PidVelKi  = 5
 motor1.PidTrqKp  = 30
 motor1.PidTrqKp  = 5
-motor1.Fn31()
-motor1.acceleration = 1000
-motor1.Fn34()
-motor1.Fn30()
-motor1.Fn33()
+motor1.Fn31() # write PID to Ram
+# Specify desired accelration
+motor1.acceleration = 1000 
+motor1.Fn34() # write acceleration to Ram
+
+motor1.Fn30() # read PID
+motor1.Fn33() # read acceleration
 motor1.info()
 
-motor2 = RMD.RMD(0x141,bus,ratio = 13) #shoulder
+
+# ---------- RMD motor with ID 2 (Shoulder) ----------
+motor2 = RMD.RMD(0x141,bus,ratio = 13) # Shoulder
 
 motor2.info()
 
-motor2.Fn30()
-motor2.Fn33()
+motor2.Fn30() # read PID
+motor2.Fn33() # read acceleration
 motor2.info()
-
+# Specify desired PID
 motor2.PidPosKp  = 30
 motor2.PidPosKi  = 0
 motor2.PidVelKp  = 30
 motor2.PidVelKi  = 5
 motor2.PidTrqKp  = 30
 motor2.PidTrqKp  = 5
-motor2.Fn31()
-motor2.acceleration = 1000
-motor2.Fn34()
-motor2.Fn30()
-motor2.Fn33()
+motor2.Fn31() # write PID to Ram
+# Specify desired accelration
+motor2.acceleration = 1000 
+motor2.Fn34() # write acceleration to Ram
+
+motor2.Fn30() # read PID
+motor2.Fn33() # read acceleration
 motor2.info()
+
+
+# ---------- Commands ----------
 
 while True:
 	angle1 = int(input("Elbow"))
