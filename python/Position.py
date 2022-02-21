b162da0fd1466541d8c51b1853b47b31472dd05f
Florian9638 <florian.dallago@aigritec.com>
2022-02-21 10:39:08 +0100

    comments

--- python/Position.py
+++ python/Position.py
@@ -2,65 +2,67 @@ import RMD
 import can
 import time
 import numpy as np
-import kinematics_web
+import kinematics
 
 # Using specific buses works similar:
 bus = can.interface.Bus(bustype='socketcan', channel='can0', bitrate=1000000)
 
 # Variables:
-a = 500 # Motors acceleration
-v = 500 # Motors velocity
-t = 3 # Waiting time
+a = 500  # Motors acceleration
+v = 500  # Motors velocity
+t = 3  # Waiting time
 
 # ---------- RMD motor with ID 1 (Elbow) ----------
-motor_E = RMD.RMD(0x142,bus,ratio = 13.5) # Elbow
+motor_E = RMD.RMD(0x142, bus, ratio=13.5)  # Elbow
 
-#motor_E.info() # 'info' prints Pid and Acceleration 
+#motor_E.info() # 'info' prints Pid and Acceleration
 
-motor_E.Fn30() # read PID
+motor_E.Fn30()  # read PID
 
-motor_E.Fn33() # read acceleration
+motor_E.Fn33()  # read acceleration
 #motor_E.info()
 # Specify desired PID
-motor_E.PidPosKp  = 30 
-motor_E.PidPosKi  = 0
-motor_E.PidVelKp  = 30
-motor_E.PidVelKi  = 5
-motor_E.PidTrqKp  = 30
-motor_E.PidTrqKp  = 5
-motor_E.Fn31() # write PID to Ram
+motor_E.PidPosKp = 30
+motor_E.PidPosKi = 0
+motor_E.PidVelKp = 30
+motor_E.PidVelKi = 5
+motor_E.PidTrqKp = 30
+motor_E.PidTrqKp = 5
+motor_E.Fn31()  # write PID to Ram
 # Specify desired accelration
 motor_E.acceleration = a
-motor_E.Fn34() # write acceleration to Ram
+motor_E.Fn34()  # write acceleration to Ram
 
 
 # ---------- RMD motor with ID 2 (Shoulder) ----------
-motor_S = RMD.RMD(0x141,bus,ratio = 13.5) # Shoulder
+motor_S = RMD.RMD(0x141, bus, ratio=13.5)  # Shoulder
 
 #motor_S.info()
 
-motor_S.Fn30() # read PID
-motor_S.Fn33() # read acceleration
+motor_S.Fn30()  # read PID
+motor_S.Fn33()  # read acceleration
 #motor_S.info()
 # Specify desired PID
-motor_S.PidPosKp  = 30
-motor_S.PidPosKi  = 0
-motor_S.PidVelKp  = 30
-motor_S.PidVelKi  = 5
-motor_S.PidTrqKp  = 30
-motor_S.PidTrqKp  = 5
-motor_S.Fn31() # write PID to Ram
+motor_S.PidPosKp = 30
+motor_S.PidPosKi = 0
+motor_S.PidVelKp = 30
+motor_S.PidVelKi = 5
+motor_S.PidTrqKp = 30
+motor_S.PidTrqKp = 5
+motor_S.Fn31()  # write PID to Ram
 # Specify desired accelration
 motor_S.acceleration = a
-motor_S.Fn34() # write acceleration to Ram
+motor_S.Fn34()  # write acceleration to Ram
 
 
 while True:
 	# choose a target position to compute the joint angles (IK)
-	x = float(input("x-axis [mm]: ")) # x is along the straight arm (extiting the machine)
-	y = float(input("y-axis [mm]: ")) # y is perpendicular to the straight arm (positive is towards the coffee machine)
+	# x is along the straight arm (extiting the machine)
+	x = float(input("x-axis [mm]: "))
+	# y is perpendicular to the straight arm (positive is towards the coffee machine)
+	y = float(input("y-axis [mm]: "))
 	target = np.array((x, y))
-	ik = kinematics_web.IK(target, elbow=1)
+	ik = kinematics.IK(target, elbow=1)
 	print("The following solutions should reach endpoint position %s: %s" % (target, ik))
 
 	#print(ik[0]) #theta_shoulder
@@ -68,6 +70,19 @@ while True:
 
 	motor_S.goG(ik[0], v)
 	motor_E.goG(ik[1]+ik[0], v)
+	time.sleep(t)
+
+
+while False:
+	# choose desired joint angles to compute x,y coordinates (DK)
+	theta1 = float(input("Shoulder angle [deg]: "))
+	theta2 = float(input("Elbow angle [deg]: "))
+	target = np.array((theta1, theta2))
+	dk = kinematics.DK(target)
+	print("The following angles %s, correspond to x,y coord: %s" % target % dk)
+
+	motor_S.goG(dk[0], v)
+	motor_E.goG(dk[1]+dk[0], v)
 	time.sleep(t)
 
 '''
---
+++ python/kinematics.py
@@ -0,0 +1,99 @@
+import math
+import numpy as np
+
+
+'''
+Direct kinematics [DK]:
+Transforms joint angles (theta1,theta2) in eef coordinates (x,y))
+'''
+
+
+def DK(target, len1=497.0, len2=500.0):
+    # target = (theta1, theta2)
+    target[0] = np.deg2rad(target[0])
+    target[1] = np.deg2rad(target[1])
+    x = len1*np.cos(target[0]) + len2*np.cos(target[0]+target[1])
+    y = len1*np.sin(target[0]) + len2*np.sin(target[0]+target[1])
+    x = round(x, 2)  # round to 2 decimal numbers
+    y = round(y, 2)
+    print("x: " + str(x), "y: " + str(y))
+    return x, y
+
+
+theta1 = float(input("MT shoulder [deg]: "))
+theta2 = float(input("MT elbow [deg]: "))
+target = np.array((theta1, theta2))
+dk = DK(target)
+print("The following x,y coord should return angles %s: %s" % (target, dk))
+
+
+'''
+Inverse kinematics [IK]:
+Transforms eef coordinates (x,y) in joint angles (theta1,theta2)
+'''
+
+
+def IK(target, len1=497.0, len2=500.0, elbow=0):
+
+    # find the position of the point in polar coordinates
+    radiussq = np.dot(target, target)
+    # radius = distance from the origin to the eef
+    radius = math.sqrt(radiussq)
+
+    # theta is the angle of target point w.r.t. X axis
+    theta = math.atan2(target[1], target[0])
+
+    # use the law of cosines to compute the elbow angle theta2
+    #   R**2 = l1**2 + l2**2 - 2*l1*l2*cos(pi - theta2)
+    #   beta = pi - theta2
+    #   both theta2 and -theta2 are valid solutions
+    cos_beta = (radiussq - len1**2 - len2**2) / (-2 * len1 * len2)
+    if cos_beta < -1.0:
+        beta = math.pi
+    elif cos_beta > 1.0:
+        beta = 0.0
+    else:
+        beta = math.acos(cos_beta)
+
+    # use the law of sines to compute the angle alpha
+    #  radius / sin(beta)  = l2 / sin(alpha)
+    if radius > 0.0:
+        alpha = math.asin(len2 * math.sin(beta) / radius)
+    else:
+        alpha = 0.0
+
+    #  compute the two solutions with opposite elbow sign
+    theta1_1 = np.rad2deg(theta - alpha)
+    theta2_1 = np.rad2deg(math.pi - beta)
+    theta1_1 = round(theta1_1, 2)
+    theta2_1 = round(theta2_1, 2)
+    soln1 = np.array((theta1_1, theta2_1))
+
+    theta1_2 = np.rad2deg(theta + alpha)
+    theta2_2 = np.rad2deg(beta - math.pi)
+    theta1_2 = round(theta1_2, 2)
+    theta2_2 = round(theta2_2, 2)
+    soln2 = np.array((theta1_2, theta2_2))
+
+# Solutions in rad:
+#    soln1 = np.array((theta - alpha, math.pi - beta))
+#    soln2 = np.array((theta + alpha, beta - math.pi))
+
+    if elbow == 1:
+        return soln1
+    else:
+        return soln2
+
+
+'''
+################################################################
+# When run as a script, try a sample problem:
+if __name__ == "__main__":
+
+    # choose a sample target position to test IK
+	x = float(input("x-axis [mm]: "))
+	y = float(input("y-axis [mm]: "))
+	target = np.array((x, y))
+	ik = IK(target)
+	print("The following solutions should reach endpoint position %s: %s" % (target, ik))
+'''
