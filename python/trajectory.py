1742082d93af5737ce58180263dc6655fc2d9e9f
Florian9638 <florian.dallago@aigritec.com>
2022-02-22 10:35:05 +0100

    3 case trajectory

--- python/trajectory.py
+++ python/trajectory.py
@@ -1,37 +1,37 @@
 import numpy as np
 
-x1 = 200
-y1 = 200
+'''
+Current pose is obtained from DK:
+Read joint angles(theta1,theta2)-->Compute DK(x1,y1)
+'''
+x1 = 800.0
+y1 = 800.0
+'''
+New pose is specified by us: (x2,y2)
+'''
+x2 = 300.0
+y2 = 800.0
 
-x2 = 800
-y2 = 800
+# Definition of the path points:
+steps_x = np.linspace(x1, x2, 20, endpoint=True) # np.linspace(start, stop, num)
+steps_y = np.linspace(y1, y2, 20, endpoint=True)
 
-# define starting point
-start = []
-# define end point
-end = []
+if x1==x2:
+	for y in steps_y:
+		x = x1
+		x = round(x,2)
+		y = round(y,2)
+		print(x,y)
+elif y1==y2:
+	for x in steps_x:
+		y = y1
+		x = round(x,2)
+		y = round(y,2)
+		print(x,y)
+else:
+	for x in steps_x:
+		y = (((x-x1)/(x2-x1))*(y2-y1))+y1
+		x = round(x,2)
+		y = round(y,2)
+		print(x,y)
 
-np.append(x1)
-np.append(y1)
-
-np.append(x2, end)
-np.append(y2, end)
-
-#start[0] = x1
-#start[1] = y1
-
-x = x1
-
-while True:
-	y = (((x-x1)/(x2-x1))*(y2-y1))+y1
-	print(x, y)
-	x = x + 50
-	if x > x2:
-		break
-		
-while False:
-	y = (((x-start[0])/(end[0]-start[0]))*(end[1]-start[1]))+start[1]
-	print(x, y)
-	x = x + 50
-	if x > end[1]:
-		break
