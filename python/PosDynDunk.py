857c2a737daa1d30f109c2c219ade9ddc09222f3
dfacchin <dfacchin@gmail.com>
2022-03-30 18:03:04 +0200

    test with dunker and Dyn

---
+++ python/PosDynDunk.py
@@ -0,0 +1,214 @@
+import RMD
+import can
+import time
+import numpy as np
+import kinematics
+
+############## DUNKER
+import socket
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+host = "10.170.43.203"
+port = 8893
+def movDunker(pos):
+	global sock
+	request = "G:" + str(pos) + ":" + str(velocity)
+    # send the message
+    data = bytes(str(request).encode("utf8"))
+    sock.sendto(data, (host, port))
+
+    # empty buffer
+    sock.recv(1024)
+
+
+
+import os
+
+if os.name == 'nt':
+    import msvcrt
+    def getch():
+        return msvcrt.getch().decode()
+else:
+    import sys, tty, termios
+    fd = sys.stdin.fileno()
+    old_settings = termios.tcgetattr(fd)
+    def getch():
+        try:
+            tty.setraw(sys.stdin.fileno())
+            ch = sys.stdin.read(1)
+        finally:
+            termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
+        return ch
+
+from dynamixel_sdk import * # Uses Dynamixel SDK library
+
+#********* DYNAMIXEL Model definition *********
+#***** (Use only one definition at a time) *****
+MY_DXL = 'X_SERIES'       # X330 (5.0 V recommended), X430, X540, 2X430
+# MY_DXL = 'MX_SERIES'    # MX series with 2.0 firmware update.
+# MY_DXL = 'PRO_SERIES'   # H54, H42, M54, M42, L54, L42
+# MY_DXL = 'PRO_A_SERIES' # PRO series with (A) firmware update.
+# MY_DXL = 'P_SERIES'     # PH54, PH42, PM54
+# MY_DXL = 'XL320'        # [WARNING] Operating Voltage : 7.4V
+
+
+ADDR_TORQUE_ENABLE          = 64
+ADDR_GOAL_POSITION          = 116
+ADDR_PRESENT_POSITION       = 132
+DXL_MINIMUM_POSITION_VALUE  = 0         # Refer to the Minimum Position Limit of product eManual
+DXL_MAXIMUM_POSITION_VALUE  = 4095      # Refer to the Maximum Position Limit of product eManual
+BAUDRATE                    = 57600
+
+# DYNAMIXEL Protocol Version (1.0 / 2.0)
+# https://emanual.robotis.com/docs/en/dxl/protocol2/
+PROTOCOL_VERSION            = 2.0
+
+# Factory default ID of all DYNAMIXEL is 1
+DXL_ID                      = 1
+# Use the actual port assigned to the U2D2.
+# ex) Windows: "COM*", Linux: "/dev/ttyUSB*", Mac: "/dev/tty.usbserial-*"
+DEVICENAME                  = '/dev/ttyUSB0'
+
+TORQUE_ENABLE               = 1     # Value for enabling the torque
+TORQUE_DISABLE              = 0     # Value for disabling the torque
+DXL_MOVING_STATUS_THRESHOLD = 20    # Dynamixel moving status threshold
+
+index = 0
+dxl_goal_position = [DXL_MINIMUM_POSITION_VALUE, DXL_MAXIMUM_POSITION_VALUE]         # Goal position
+
+
+# Initialize PortHandler instance
+# Set the port path
+# Get methods and members of PortHandlerLinux or PortHandlerWindows
+portHandler = PortHandler(DEVICENAME)
+
+# Initialize PacketHandler instance
+# Set the protocol version
+# Get methods and members of Protocol1PacketHandler or Protocol2PacketHandler
+packetHandler = PacketHandler(PROTOCOL_VERSION)
+
+# Open port
+if portHandler.openPort():
+    print("Succeeded to open the port")
+else:
+    print("Failed to open the port")
+    print("Press any key to terminate...")
+    getch()
+    quit()
+
+
+# Set port baudrate
+if portHandler.setBaudRate(BAUDRATE):
+    print("Succeeded to change the baudrate")
+else:
+    print("Failed to change the baudrate")
+    print("Press any key to terminate...")
+    getch()
+    quit()
+
+# Enable Dynamixel Torque
+dxl_comm_result, dxl_error = packetHandler.write1ByteTxRx(portHandler, DXL_ID, ADDR_TORQUE_ENABLE, TORQUE_ENABLE)
+if dxl_comm_result != COMM_SUCCESS:
+    print("%s" % packetHandler.getTxRxResult(dxl_comm_result))
+elif dxl_error != 0:
+    print("%s" % packetHandler.getRxPacketError(dxl_error))
+else:
+    print("Dynamixel has been successfully connected")
+
+
+def moveDyn(angle):
+	global packetHandler,portHandler,DXL_ID,ADDR_TORQUE_ENABLE,TORQUE_ENABLE
+	#ANGLE IS IN DEGREES
+	#4096 = 360 DEGREES
+	posReq = int((angle *4096 ) / 360)
+	dxl_comm_result, dxl_error = packetHandler.write4ByteTxRx(portHandler, DXL_ID, ADDR_GOAL_POSITION, posReq)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# Using specific buses works similar:
+bus = can.interface.Bus(bustype='socketcan', channel='can0', bitrate=1000000)
+
+# Variables:
+a = 1500  # Motors acceleration
+v = 1500  # Motors velocity
+t = 3  # Waiting time
+
+# ---------- RMD motor with ID 1 (Elbow) ----------
+motor_E = RMD.RMD(0x142, bus, ratio=13.5)  # Elbow
+
+#motor_E.info() # 'info' prints Pid and Acceleration
+
+motor_E.Fn30()  # read PID
+
+motor_E.Fn33()  # read acceleration
+#motor_E.info()
+# Specify desired PID
+motor_E.PidPosKp = 30
+motor_E.PidPosKi = 0
+motor_E.PidVelKp = 30
+motor_E.PidVelKi = 5
+motor_E.PidTrqKp = 30
+motor_E.PidTrqKp = 5
+motor_E.Fn31()  # write PID to Ram
+# Specify desired accelration
+motor_E.acceleration = a
+motor_E.Fn34()  # write acceleration to Ram
+
+
+# ---------- RMD motor with ID 2 (Shoulder) ----------
+motor_S = RMD.RMD(0x141, bus, ratio=13.5)  # Shoulder
+
+#motor_S.info()
+
+motor_S.Fn30()  # read PID
+motor_S.Fn33()  # read acceleration
+#motor_S.info()
+# Specify desired PID
+motor_S.PidPosKp = 30
+motor_S.PidPosKi = 0
+motor_S.PidVelKp = 30
+motor_S.PidVelKi = 5
+motor_S.PidTrqKp = 30
+motor_S.PidTrqKp = 5
+motor_S.Fn31()  # write PID to Ram
+# Specify desired accelration
+motor_S.acceleration = a
+motor_S.Fn34()  # write acceleration to Ram
+
+
+while True:
+	# choose a target position to compute the joint angles (IK)
+	# x is along the straight arm (extiting the machine)
+	x = float(input("x-axis [mm]: "))
+	# y is perpendicular to the straight arm (positive is towards the coffee machine)
+	y = float(input("y-axis [mm]: "))
+	z = float(input("z-axis [mm]: "))
+	target = np.array((x, y))
+	ik = kinematics.IK(target, elbow=1)
+	print("The following solutions should reach endpoint position %s: %s" % (target, ik))
+	#print(ik[0]) #theta_shoulder
+	#print(ik[1]) #theta_elbow
+	motor_S.goG(ik[0], v)
+	motor_E.goG(ik[1]+ik[0], v)
+	#Dynamixel
+	AngoloDyn = ik[1] - ik[0] #Dopo 1000 anni di ROS, angolo wrist è Angolo gomitino - spalluccia
+	moveDyn(AngoloDyn)
+	moveDunker(int(z))
+
+
+	time.sleep(t)
