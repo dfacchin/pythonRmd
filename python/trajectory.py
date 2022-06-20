bb148f62a7aad744562233520f10f39266f55988
Florian9638 <florian.dallago@aigritec.com>
2022-06-20 09:27:33 +0200

    file for arm trajectory

---
+++ python/trajectory.py
@@ -0,0 +1,19 @@
+# Variables
+
+# time
+#t # [s] timthis is not a fixed value, it represents the time that goes by
+tf = 4 # [s] time in which we want the motion to be executed
+
+# position
+
+
+
+# velocity
+
+# acceleration
+
+# Cubic Polinomial [Trajectory of angular displacement]
+
+theta = theta_i + theta_d_i 
+
+# Cubic Polinomial [Trajectory of angular velocity]
