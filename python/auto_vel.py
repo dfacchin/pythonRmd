b9a205d1fdd9d17e2e302ef690b3ba6fdc162762
Florian9638 <florian.dallago@aigritec.com>
2022-06-29 10:45:27 +0200

    create a loop for theta and theta_d

--- python/auto_vel.py
+++ python/auto_vel.py
@@ -1,27 +1,17 @@
 import numpy as np
 
-# Variables
-'''
-pos_target_i
-pos_target_f
-vel_target_i
-vel_target_f
-ti
-tf
-steps
-'''
 
 # Initial Conditions (i.c.):
 # Define (time,pose,vel) for each path point
-t = [0, 2, 4, 8, 10] # [s]
+time = [0, 2, 4, 8, 10] # [s]
 theta = [10, 20, 0, 30, 40] # [deg]
 theta_d = [0] # [deg/s]
 
 # Velocity in each segment
 v = [] # create an empty list
-for index, (elem_t,elem_theta) in enumerate(zip(t,theta)):
-    if (index+1 < len(t)): # compute only until the second to last value
-        v.append((theta[index+1]-theta[index])/(t[index+1]-t[index]))
+for index, (elem_t,elem_theta) in enumerate(zip(time,theta)):
+    if (index+1 < len(time)): # compute only until the second to last value
+        v.append((theta[index+1]-theta[index])/(time[index+1]-time[index]))
 
 # Velocity at each path point
 for index, elem in enumerate(v):
@@ -33,28 +23,24 @@ for index, elem in enumerate(v):
 theta_d.append(0) # this represents the endpoint-velocity (i.c.)
 
 
-
-'''
 # ----------------------------------------------------------
 # Solve the Cubic Polinomial for each step:
-# Time
-t = np.linspace(ti, tf, steps) # [s]
 
-# Delta values (Shoulder)
-delta_theta_S = pos_target_f[0] - pos_target_i[0]
-theta_d_sum_S = vel_target_f[0] + vel_target_i[0]
-# c-coefficients
-# Shoulder
-c0_S = pos_target_i[0]
-c1_S = vel_target_i[0]
-c2_S = ((3*delta_theta_S)/(tf-ti)**2) - ((2*vel_target_i[0])/(tf-ti)) - (vel_target_f[0]/(tf-ti))
-c3_S = (-(2*delta_theta_S)/(tf-ti)**3) + (theta_d_sum_S/(tf-ti)**2)
+for index, (el_time,el_theta,el_theta_d) in enumerate(zip(time,theta,theta_d)):
+    t = np.linspace(time[index], time[index+1], steps) # [s] Time
+    delta_theta_S = theta[index+1] - theta[index] # Delta value
+    sum_theta_d_S = theta_d[index+1] + theta_d[index] # Sum
 
-# Cubic Polinomial [Trajectory of angular displacement]
-theta_S = c0_S + (c1_S*(t-ti)) + (c2_S*(t-ti)**2) + (c3_S*(t-ti)**3) # [deg] Shoulder
-# Cubic Polinomial [Trajectory of angular velocity]
-theta_d_S = c1_S + (2*c2_S*(t-ti)) + (3*c3_S*(t-ti)**2) # [deg/s] Shoulder
+    # c-coefficients
+    c0_S = theta[index]
+    c1_S = theta_d[index]
+    c2_S = ((3*delta_theta_S)/(time[index+1]-time[index])**2) - ((2*theta_d[index])/(time[index+1]-time[index])) - (theta_d[index]/(time[index+1]-time[index]))
+    c3_S = (-(2*delta_theta_S)/(time[index+1]-time[index])**3) + (sum_theta_d_S/(time[index+1]-time[index])**2)
 
-# Note: the variables that this function returns are arrays
-return theta_S, theta_d_S
-'''
+    # Cubic Polinomial [Trajectory of angular displacement]
+    theta_S = c0_S + (c1_S*(t-time[index])) + (c2_S*(t-time[index])**2) + (c3_S*(t-time[index])**3) # [deg] Shoulder
+    # Cubic Polinomial [Trajectory of angular velocity]
+    theta_d_S = c1_S + (2*c2_S*(t-time[index])) + (3*c3_S*(t-time[index])**2) # [deg/s] Shoulder
+
+    # Note: the variables that this function returns are arrays
+    #return theta_S, theta_d_S
