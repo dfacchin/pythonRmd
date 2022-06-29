2151fce179f37f2c1627e85c2641fb90adfe7d07
Florian9638 <florian.dallago@aigritec.com>
2022-06-29 12:04:32 +0200

    calculate correct C-coeff

--- python/auto_vel_new_C.py
+++ python/auto_vel_new_C.py
@@ -30,8 +30,8 @@ steps = 6
 for index, (el_time,el_theta,el_theta_d) in enumerate(zip(time,theta,theta_d)):
     if (index+1 < len(theta)):
         t = np.linspace(time[index], time[index+1], steps) # [s] Time
-        delta_theta_S = theta[index+1] - theta[index] # Delta value
-        sum_theta_d_S = theta_d[index+1] + theta_d[index] # Sum
+        delta_theta_S = theta[index] - theta[index+1] # Delta value
+        sum_theta_d_S = theta_d[index] + theta_d[index+1] # Sum
 
         print("theta[index]: " + str(theta[index]))
         print("theta[index+1]: " + str(theta[index+1]))
@@ -41,8 +41,8 @@ for index, (el_time,el_theta,el_theta_d) in enumerate(zip(time,theta,theta_d)):
         # c-coefficients
         c0_S = theta[index]
         c1_S = theta_d[index]
-        c2_S = ((3*delta_theta_S)/(time[index+1]-time[index])**2) - ((2*theta_d[index])/(time[index+1]-time[index])) - (theta_d[index]/(time[index+1]-time[index]))
-        c3_S = (-(2*delta_theta_S)/(time[index+1]-time[index])**3) + (sum_theta_d_S/(time[index+1]-time[index])**2)
+        c2_S = ((-3*(theta[index]-theta[index+1])) - (2*theta_d[index] + theta_d[index+1]) * (time[index+1]-time[index])) / (time[index+1]-time[index])**2
+        c3_S = ((2*(theta[index]-theta[index+1])) + (theta_d[index] + theta_d[index+1]) * (time[index+1]-time[index])) / (time[index+1]-time[index])**3
 
         print("c0_S: " + str(c0_S))
         print("c1_S: " + str(c1_S))
