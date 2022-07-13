eeb78f247edb070579d880aa7da2628d348eb21e
Florian9638 <florian.dallago@aigritec.com>
2022-07-13 14:53:12 +0200

    read MTangle

--- python/robot_control.py
+++ python/robot_control.py
@@ -107,7 +107,7 @@ while idx < len(angle_S[0]):
 	read_angle_S = motor_S.get_actual_angle()
 	read_angle_E = motor_E.get_actual_angle()
 
-	actual_angle_S = np.concatenate([actual_angle_S, -read_angle_S])
+	actual_angle_S = np.concatenate([actual_angle_S, read_angle_S])
 	actual_angle_E = np.concatenate([actual_angle_E, read_angle_E])
 
 
