1a7ad1508d8595a684f067cca0563ac8d49167a9
Florian9638 <florian.dallago@aigritec.com>
2022-07-19 11:43:03 +0200

    test motor v3

---
+++ python/gyems_v3_comunication.py
@@ -0,0 +1,35 @@
+import numpy as np
+import can
+import time
+
+import RMD
+import kinematics
+from trajectory_planner import Joint
+import plots
+
+
+# Specify the bus
+bus = can.interface.Bus(bustype='socketcan', channel='can0', bitrate=1000000)
+
+# Variables
+ratio = 9 # Gear ratio
+#v = 1000  # Motors velocity
+a = 1000  # Motors acceleration
+
+
+
+# ---------- RMD motor with ID 1 ----------
+motor_E = RMD.RMD(0x141, bus, ratio)
+motor_E.Fn30()  # read PID
+#motor_E.Fn33()  # read acceleration
+# Specify desired PID
+motor_E.PidPosKp = 30
+motor_E.PidPosKi = 5
+motor_E.PidVelKp = 30
+motor_E.PidVelKi = 30
+motor_E.PidTrqKp = 5
+motor_E.PidTrqKi = 5
+motor_E.Fn31()  # write PID to Ram
+# Specify desired acceleration
+#motor_E.acceleration = a
+#motor_E.Fn34()  # write acceleration to Ram
