26adff55ae9fc4ada1943b30d83829bd76c8dba4
Florian9638 <florian.dallago@aigritec.com>
2022-08-06 18:54:06 +0200

    start implementing the gripper movements

---
+++ python/movements/move_gripper.py
@@ -0,0 +1,28 @@
+import dynamixel as dyn
+
+'''
+This script is used to make the gripper move
+'''
+
+def open():
+    '''
+    Method used to open the gripper fingers,
+    in order to release the apple.
+    '''
+    pass
+
+
+def close():
+    '''
+    Method used to close the gripper fingers,
+    in order to grasp the apple.
+    '''
+    pass
+
+
+def twist():
+    '''
+    Method used to make the gripper fingers twist,
+    in order to cut the stalk.
+    '''
+    pass
--- python/tests/move_test.py
+++ python/tests/move_test.py
@@ -1,5 +1,5 @@
 import numpy as np
-import move
+import python.movements.move_arm as move_arm
 
 
 # Predefined positions
@@ -9,5 +9,5 @@ pick_offset = 200 # [mm]
 drop_offset = 200 # [mm]
 apple_coords = [[700,-200],[700,200],[800,300],[650,350]]
 
-path = move.pick_move(home, drop, apple_coords, pick_offset, drop_offset)
+path = move_arm.pick_move(home, drop, apple_coords, pick_offset, drop_offset)
 print("path: ", path)
